529212a0b6177cc32d6bbb8bce00bf749f14665e
brst1118-cpu <brst1118@gmail.com>
2026-05-30 05:46:59 +0000

    Complete 1st week C# fundamentals

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿int playerHP = 100;
+int playerFuel = 0;
+
+Console.WriteLine("---- 비행기 내구도와 연료 연산---");
+
+// 1. 논리연산자 활용하기
+
+if (playerHP == 0 || playerFuel == 0)
+{
+    Console.WriteLine("비행기 추락!");
+}
+else
+{
+    if (playerHP > 50 && playerFuel >50)
+    {
+        Console.WriteLine("상태와 연료량 양호!");
+    }
+    else
+    {
+        Console.WriteLine("비행시 주의필요!");
+    }
+}
